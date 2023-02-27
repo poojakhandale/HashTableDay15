9586a703b835420b693164ac280f28d4beb49add
poojakhandale <poojakhandale2000@gmail.com>
2023-02-27 20:47:28 +0530

    Remove given word

--- Program.cs
+++ Program.cs
@@ -1,37 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HashTable;
 
-class Program
-{
-    static void Main(string[] args)
+
+    class Program
     {
-        string sentence = "To be or not to be";
-        string[] words = sentence.ToLower().Split(' ');
-
-        MyLinkedList<string, int> linkedList = new MyLinkedList<string, int>(words.Length);
-
-        foreach (string word in words)
+        public static void Main(string[] args)
         {
-            int frequency = linkedList.Get(word);
-            if (frequency == 0)
-            {
-                linkedList.Add(word, 1);
-            }
-            else
-            {
-                linkedList.Add(word, frequency + 1);
+        
+            
+                WordFrequency.Frequency();
             }
         }
-
-        for (int i = 0; i < words.Length; i++)
-        {
-            int frequency = linkedList.Get(words[i]);
-            if (frequency != 0)
-            {
-                Console.WriteLine($"'{words[i]}' occurs {frequency} times.");
-                linkedList.Add(words[i], 0);
-            }
-        }
-    }
-}
-
+    
--- WordFrequency.cs
+++ WordFrequency.cs
@@ -6,38 +6,48 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HashTable
-{public  class WordFrequency
+{
+    public class WordFrequency
     {
-      
-            public static void Frequency()
+        public static void Frequency()
+        {
+            string sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            Hashtable wordFreq = new Hashtable();
+
+
+            string[] words = sentence.Split(' ');
+
+
+            foreach (string word in words)
             {
-                string sentence = "To be or not to be";
-                Hashtable wordFreq = new Hashtable();
-
-
-                string[] words = sentence.Split(' ');
-
-
-                foreach (string word in words)
+                if (wordFreq.ContainsKey(word))
                 {
-                    if (wordFreq.ContainsKey(word))
-                    {
-                        int count = (int)wordFreq[word];
-                        wordFreq[word] = count + 1;
-                    }
-                    else
-                    {
-                        wordFreq.Add(word, 1);
-                    }
+                    int count = (int)wordFreq[word];
+                    wordFreq[word] = count + 1;
                 }
-
-                foreach (string word in wordFreq.Keys)
+                else
                 {
-                    Console.WriteLine("{0}: {1}", word, wordFreq[word]);
+                    wordFreq.Add(word, 1);
                 }
+            }
+
+            foreach (string word in wordFreq.Keys)
+            {
+                Console.WriteLine("{0}: {1}", word, wordFreq[word]);
+            }
+
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("New Frequencies after removing the Word avoidable: ");
+            wordFreq.Remove("avoidable");
+
+            Console.WriteLine("---------------------------------");
+
+
+            foreach (string word in wordFreq.Keys)
+            {
+                Console.WriteLine("{0}: {1}", word, wordFreq[word]);
             }
         }
     }
-   
-    
-
+}
